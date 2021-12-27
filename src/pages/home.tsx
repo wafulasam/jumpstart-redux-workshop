23624a26c4cea1ba78b2a875b1a33bad925e889a
wafulasam <wafulasammie@gmail.com>
2021-12-27 12:23:26 +0300

    - app setup

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>redux workshop</h1>
+      <p>getting started with redux toolkit</p>
+    </div>
+  );
+};
+
+export default Home;
